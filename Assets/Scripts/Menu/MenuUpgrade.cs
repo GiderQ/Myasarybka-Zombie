7051e353fbc1c571f1368dd6f8090b6dabbd49a4
Yaroslav Davydenko <hollebeen@gmail.com>
2025-08-18 19:34:52 +0300

    fix

--- Assets/Scripts/Menu/MenuUpgrade.cs
+++ Assets/Scripts/Menu/MenuUpgrade.cs
@@ -1,39 +1,21 @@
-using TMPro;
 using UnityEngine.UI;
 using UnityEngine;
 
 public class MenuUpgrade : MonoBehaviour
 {
     public Slider slider;
-    public TextMeshProUGUI money;
 
-    private void Start()
-    {
-        ViewMoney();
-    }
     public void Upgrade()
     {
-        if (PlayerData.money >= 10)
+        if (PlayerData.money > 10)
         {
             PlayerData.money -= 10;
             slider.value++;
-            Debug.Log("Upgrade++");
-            ViewMoney();
         }
     }
 
     void ViewMoney()
     {
-        if (PlayerData.money > 0)
-        {
-            money.text = $"Money: {PlayerData.money}";
-        }
-        else money.text = "Money: 0";
-    }
-    public void DebugMoney()
-    {
-        PlayerData.money += 10;
-        ViewMoney();
-    }
 
+    }
 }
