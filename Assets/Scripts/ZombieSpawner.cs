1dcc1799bed14f7b04fa836814d06144ea78c22f
uoy8 <mrtwaterr@gmail.com>
2025-08-18 17:01:21 +0300

    Zombie Added

---
+++ Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour
+{
+    public GameObject zombiePrefab;
+    public Transform player;
+    public float spawnRate = 2f;  
+    public float spawnDistance = 10f; 
+
+    private float nextSpawnTime;
+
+    void Update()
+    {
+        if (Time.time >= nextSpawnTime)
+        {
+            SpawnZombie();
+            nextSpawnTime = Time.time + spawnRate;
+        }
+    }
+
+    void SpawnZombie()
+    {
+        if (player == null) return;
+
+        Vector2 spawnDir = Random.insideUnitCircle.normalized;
+        Vector3 spawnPos = player.position + (Vector3)(spawnDir * spawnDistance);
+        Instantiate(zombiePrefab, spawnPos, Quaternion.identity);
+    }
+}
