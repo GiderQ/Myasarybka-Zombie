421c75fa4b0c5b75c5c23a9bef623c7bae310d41
Yaroslav Davydenko <hollebeen@gmail.com>
2025-08-22 01:15:25 +0300

    Update PlayerController
    Update code

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,84 +4,81 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 4;
-    public float hpValue = PlayerData.upgradeHp;
+    int defaultSpeed = 4,
+        defaultHp = 9,
+        defaultAtack = 3;
+    float defaultSpeedAtack = 0.3f;
 
-    public bool isAlive = true;
-    public float fireRate = 0.2f;
-    private float nextFireTime = 0f;
-    public bool cooldown = true;
     public GameObject bulletPrefab, game, deathScene;
     public TextMeshProUGUI hpText,scoreText;
     public Transform firePoint;
     public Vector3 gunOffset = new Vector3(0.5f, 0, 0);
+
     public static int money, score;
+
+    bool cooldown = true;
+
+    private float nextFireTime = 0f;
+    private Rigidbody2D rb;
 
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Confined;
     }
-
-    
-
-
-
     void Update()
     {
-        Move();
         HandleShooting();    
 
         if (firePoint != null)
             firePoint.parent.localPosition = gunOffset;
         
-        if (hpValue <= 0)
+        if (defaultHp <= 0)
         {
             game.SetActive(false);
             deathScene.SetActive(true);
         }
 
-        hpText.text = ($"HP: {hpValue}");
+        hpText.text = ($"HP: {defaultHp}");
         scoreText.text = ($"Score: {score}");
     }
 
-    void Move()
+    void FixedUpdate()
     {
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveX, moveY);
-        transform.Translate(movement * speed * Time.deltaTime);
+
+        rb.MovePosition(rb.position + movement * defaultSpeed * Time.fixedDeltaTime);
     }
 
     void HandleShooting()
     {
         if (Input.GetButton("Fire1") && Time.time >= nextFireTime)
         {
-            Shoot();
-            nextFireTime = Time.time + fireRate;
+            Quaternion rotation = firePoint.rotation;
+            Instantiate(bulletPrefab, firePoint.position, rotation);
+            nextFireTime = Time.time + defaultSpeedAtack;
         }
     }
 
-    void Shoot()
-    {
-        Quaternion rotation = firePoint.rotation;
-        Instantiate(bulletPrefab, firePoint.position, rotation);
-    }
 
     private void OnCollisionStay2D(Collision2D other)
-    {   
+    {
+
         if (other.gameObject.CompareTag("Zombie") && cooldown)
         {
             SpriteRenderer color = GetComponent<SpriteRenderer>();
-            cooldown = false;
+             cooldown = false;
             StartCoroutine(GetDamage(color));
         }
     }
 
     IEnumerator GetDamage(SpriteRenderer color)
     {
-        hpValue -= 3;
+        defaultHp -= 3;
         color.color = Color.red;
         yield return new WaitForSeconds(0.1f);
         color.color = Color.white;
